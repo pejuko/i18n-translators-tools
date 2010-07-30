20bf86825442f794a8a6284e7d13cf3538f333e3
Petr Kovar <pejuko@gmail.com>
2010-07-30 13:01:52 +0200

    fix: migrate works with new hash_to_keys; more comments

--- lib/i18n/processor.rb
+++ lib/i18n/processor.rb
@@ -12,10 +12,12 @@ module I18n::Translate
       attr_reader :processors
     end
 
+    # append processor to the registry
     def self.<<(processor)
       @processors << processor
     end
 
+    # find processor for fname and use it to read data
     def self.read(fname, tr)
       processor = find_processor(fname)
       raise "Unknown file format" unless processor
@@ -23,6 +25,7 @@ module I18n::Translate
       worker.read
     end
 
+    # find processor for fname and use it to save data
     def self.write(fname, data, tr)
       processor = find_processor(fname)
       raise "Unknown file format `#{fname}'" unless processor
@@ -30,6 +33,7 @@ module I18n::Translate
       worker.write(data)
     end
 
+    # find appropriate processor for given file name
     def self.find_processor(fname)
       @processors.each do |processor|
         return processor if processor.can_handle?(fname)
@@ -37,6 +41,7 @@ module I18n::Translate
       nil
     end
 
+    # register formats from all known processors
     def self.init(default='yml')
       @processors.each do |processor|
         processor.register(default)
@@ -44,15 +49,19 @@ module I18n::Translate
     end
 
 
+    # this is abstract class for processors. processors should mainly implement
+    # protected methdos import and export
     class Template
       FORMAT = []
 
+      # register new processor
       def self.inherited(processor)
         Processor << processor
       end
 
       attr_reader :filename, :translate
 
+      # initialize new processor
       def initialize(fname, tr)
         @filename = fname
         @translate = tr
@@ -60,6 +69,7 @@ module I18n::Translate
         @lang = $1.to_s.strip
       end
 
+      # register proessors's formats
       def self.register(default='yml')
         self::FORMAT.each do |format|
           unless I18n::Translate::FORMATS.include?(format)
@@ -73,6 +83,7 @@ module I18n::Translate
         end
       end
 
+      # read file into hash
       def read
         data = File.open(@filename, mode("r")) do |f|
           f.flock File::LOCK_SH
@@ -81,6 +92,7 @@ module I18n::Translate
         import(data)
       end
 
+      # write hash to file
       def write(data)
         File.open(@filename, mode("w")) do |f|
           f.flock File::LOCK_EX
@@ -88,6 +100,7 @@ module I18n::Translate
         end
       end
 
+      # check if processor can handle this file
       def self.can_handle?(fname)
         fname =~ %r{\.([^\.]+)$}i
         self::FORMAT.include?($1)
@@ -95,14 +108,17 @@ module I18n::Translate
 
     protected
 
+      # converts raw data from file to hash
       def import(data)
         data
       end
 
+      # converts hash to raw
       def export(data)
         data
       end
 
+      # converts inspected string back into normal string
       def uninspect(str)
         return nil unless str
         str.gsub(%r!\\([\\#"abefnrstvx]|u\d{4}|u\{[^\}]+\}|\d{1,3}|x\d{1,2}|cx|C-[a-zA-Z]|M-[a-zA-Z])!) do |m|
@@ -116,22 +132,29 @@ module I18n::Translate
         end
       end
 
-      # convert old and shorthand fields
+      # convert old, t shorthand fields
       def migrate(data)
         keys = I18n::Translate.hash_to_keys(data, @translate.options[:separator])
         keys.each do |key|
-          k, prop = $1, $2 if key =~ /(.*)\.([^\.]+)$/
-          next unless prop or k
-          next unless ["old", "t"].include?(prop)
-          entry = I18n::Translate.find(k, data, @translate.options[:separator])
-          value = entry.delete(prop)
-          prop = (prop == "old") ? "old_default" : "translation"
-          entry[prop] = value
-          I18n::Translate.set(k, entry, data, @translate.options[:separator])
+          entry = I18n::Translate.find(key, data, @translate.options[:separator])
+          next unless I18n::Translate.is_enhanced?(entry)
+          %w(old t).each do |prop|
+            next unless entry[prop]
+            value = entry.delete(prop)
+            prop = case(prop)
+                   when "old"
+                     "old_default"
+                   when "t"
+                     "translation"
+                   end
+            entry[prop] = value
+          end
+          I18n::Translate.set(key, entry, data, @translate.options[:separator])
         end
         data
       end
 
+      # in ruby 1.9 it sets encoding for opening IOs
       def mode(m)
         mode = m.dup
         mode << ":" << @translate.options[:encoding] if defined?(Encoding)
